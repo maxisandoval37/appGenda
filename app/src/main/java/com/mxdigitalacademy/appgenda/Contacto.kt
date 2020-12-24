cfa56a42800ca6453b31ceb7c076590c9f660e66
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-12-24 15:46:52 -0300

    maqueta

---
+++ app/src/main/java/com/mxdigitalacademy/appgenda/Contacto.kt
@@ -0,0 +1,11 @@
+package com.mxdigitalacademy.appgenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contacto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacto)
+    }
+}
