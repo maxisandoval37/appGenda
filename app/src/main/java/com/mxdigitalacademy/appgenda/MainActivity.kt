607bc64fcb6b2948784c43fe79197eaef58c2c25
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-12-24 17:14:44 -0300

    add custom toolbar

--- app/src/main/java/com/mxdigitalacademy/appgenda/MainActivity.kt
+++ app/src/main/java/com/mxdigitalacademy/appgenda/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mxdigitalacademy.appgenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
