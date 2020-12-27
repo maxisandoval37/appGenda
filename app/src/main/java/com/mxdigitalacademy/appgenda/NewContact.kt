d216f946a083806bdce0ab5a08b038509120d9cb
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-12-27 19:36:51 -0300

    back button in toolbar

--- app/src/main/java/com/mxdigitalacademy/appgenda/NewContact.kt
+++ app/src/main/java/com/mxdigitalacademy/appgenda/NewContact.kt
@@ -2,10 +2,28 @@ package com.mxdigitalacademy.appgenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class NewContact : AppCompatActivity() {
+
+    private var toolbar: Toolbar? = null
+
+    private fun hablitraBotonVolver(){
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun iniciarToolbar(){
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.title = ""
+        setSupportActionBar(toolbar)
+        hablitraBotonVolver()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_contact)
+
+        iniciarToolbar()
     }
 }
