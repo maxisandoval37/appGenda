889e5a69a3cd56727287ee43dceedc3ca9c7d0a3
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-12-27 14:46:06 -0300

    update and fix interface

--- app/src/main/java/com/mxdigitalacademy/appgenda/MainActivity.kt
+++ app/src/main/java/com/mxdigitalacademy/appgenda/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.mxdigitalacademy.appgenda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
@@ -30,15 +32,15 @@ class MainActivity : AppCompatActivity() {
         val adaptador = CustomAdapter(this,listaObjContactos)
         listaVisual.adapter = adaptador
 
-        listaVisual.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+        listaVisual.onItemClickListener = AdapterView.OnItemClickListener { _, _, i, _ ->
             Toast.makeText(this, listaObjContactos[i].getNombreCompleto(), Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun listViewElements(){
-        listaObjContactos.add(ObjContacto(R.drawable.walter_white,"Walter","White","3478598723","ww@bb.com"))
-        listaObjContactos.add(ObjContacto(R.drawable.jesse_pinkman,"Jesse","Pinkman","112783498","jesseP@gmail.com"))
-        listaObjContactos.add(ObjContacto(R.drawable.gus_fring,"Gus","Fring","1132456745","fring@gmail.com"))
+        listaObjContactos.add(ObjContacto(R.drawable.walter_white,"Walter","White","3478598723","","ww@bb.com"))
+        listaObjContactos.add(ObjContacto(R.drawable.jesse_pinkman,"Jesse","Pinkman","112783498","2734892893","jesseP@gmail.com"))
+        listaObjContactos.add(ObjContacto(R.drawable.gus_fring,"Gus","Fring","1132456745","","fring@gmail.com"))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
